a47cb38e4de0d7bde170c1dc625560dd890dc7de
Livestronger08 <derek.downie@ttu.edu>
2020-06-07 17:24:58 +0000

    Lesson 9: Luhn one error

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,13 +1,27 @@
 module Luhn
     def self.is_valid?(number)
         #WRITE YOUR SOLUTION HERE
-        luhn.is_valid?()
-        sum = 0
-
-        number.reverse.split("").each_slice(2) do |a, b|
-            sum += x.to_i + (2*y.to_i).divmod(10).sum
+        #break the credit card into its individual digits.
+        numbers = []
+        digits = number.to_s.split('').map(&:to_i)
+        # start from the right, double every second digit.
+        digits.reverse.each_with_index do |item, index|
+            if index % 2 == 0
+                double_digits = item * 2
+                numbers.push(double_digits)
+            else
+                numbers.push(item)
+            end
         end
-
-        p sum%10 == 0 ? 'valid' : 'invalid'
+        #If the doubled value is greater than or equal to 10, take the value and subtract 9 from it.
+        numbers.map do |i|
+            if i >= 10
+                i - 9
+            end
+        end
+        #sum the digits
+        sum = numbers.inject(:+)
+        #if the sum is divisible by 10, it's a valid number.  Otherwise it's invalid.
+        return sum % 10 == 0
     end
 end
