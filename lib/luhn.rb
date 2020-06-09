77b715ffbd43008a70da4e6f48a1771e147d91b2
Livestronger08 <derek.downie@ttu.edu>
2020-06-09 19:39:54 +0000

    Lesson 9: Luhn Algorithm Completed

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,27 +1,26 @@
 module Luhn
     def self.is_valid?(number)
-        #WRITE YOUR SOLUTION HERE
+        digits = Math.log(number).floor.downto(0).map { |i| (number / 10**i) % 10 }
+
         #break the credit card into its individual digits.
-        numbers = []
-        digits = number.to_s.split('').map(&:to_i)
+        digit = []
+        digits = digits.reverse
         # start from the right, double every second digit.
-        digits.reverse.each_with_index do |item, index|
-            if index % 2 == 0
-                double_digits = item * 2
-                numbers.push(double_digits)
+        digits.each_with_index do |item, index|
+            if ((index != 0) && (index % 2 != 0))
+                item *= 2
+            #If the doubled value is greater than or equal to 10, take the value and subtract 9 from it.
+                if  item >= 10
+                    item -= 9
+                end
+                digit << item
             else
-                numbers.push(item)
+                digit << item
             end
-        end
-        #If the doubled value is greater than or equal to 10, take the value and subtract 9 from it.
-        numbers.map do |i|
-            if i >= 10
-                i - 9
-            end
-        end
+        end 
         #sum the digits
-        sum = numbers.inject(:+)
+        sum = digit.inject(:+)
         #if the sum is divisible by 10, it's a valid number.  Otherwise it's invalid.
-        return sum % 10 == 0
+        (sum % 10 == 0) ? true : false 
     end
 end
